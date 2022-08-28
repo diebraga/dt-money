b966cece65a5bd53a5cd86a89e1d89422e6e4cdb
Diego Braga <diegobraga@hertz.com>
2022-08-28 18:28:23 +0100

    tables style

--- src/pages/Trasactions/index.tsx
+++ src/pages/Trasactions/index.tsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import { Header } from "../../Components/Header";
 import { Summary } from "../../Components/Summary";
 
@@ -6,6 +7,71 @@ export function Transactions() {
     <>
       <Header />
       <Summary />
+
+      <TrasanctionsContainer>
+        <TrasanctionsTable>
+          <tbody>
+            <tr>
+              <td width={'50%'}>Web development</td>
+              <td>
+                <PriceHighlight variant="income">
+                  € 1.000.00
+                </PriceHighlight>
+              </td>
+              <td>Internet</td>
+              <td>13/12/2022</td>
+            </tr>
+            <tr>
+              <td width={'50%'}>Web development</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - € 1.000.00
+                </PriceHighlight>
+              </td>
+              <td>Internet</td>
+              <td>13/12/2022</td>
+            </tr>
+          </tbody>
+        </TrasanctionsTable>
+      </TrasanctionsContainer>
     </>
   )
 }
+
+const TrasanctionsContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`
+
+const TrasanctionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+
+  /* margin-top: 1.5rem; */
+
+  td {
+    padding: 1.25rem 2rem;
+    background: ${p => p.theme["gray-700"]};
+
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+`
+
+interface PriceHighlight {
+  variant: 'income' | 'outcome'
+}
+
+const PriceHighlight = styled.span<PriceHighlight>`
+  color: ${p => p.variant === 'income' ? p.theme["gray-300"] : p.theme["red-300"]}
+`
